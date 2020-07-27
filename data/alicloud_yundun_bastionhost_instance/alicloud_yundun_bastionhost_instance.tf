b9b79b3178bcd176a9b7ad4d3c349ba8b1a480a0
rodolphe <rodolphe@petrowsky.fr>
2020-07-27 12:15:06 +0200

    new resources added

---
+++ data/alicloud_yundun_bastionhost_instance/alicloud_yundun_bastionhost_instance.tf
@@ -0,0 +1,1 @@
+# references: https://www.terraform.io/docs/providers/alicloud/d/yundun_bastionhost_instances.html 
---
+++ resources/alicloud_yundun_bastionhost_instance/alicloud_yundun_bastionhost_instance.tf
@@ -0,0 +1,1 @@
+# references: https://www.terraform.io/docs/providers/alicloud/r/yundun_bastionhost_instance.html 
